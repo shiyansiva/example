d25031d5f05ca2428823146ff59d17047a1c97f9
Sivakumar415 <sivakumar.m@capestart.com>
2023-09-20 15:52:51 +0530

    initial commit

---
+++ screens/Home.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import { showEditor } from 'react-native-video-trim';
+import { launchImageLibrary } from 'react-native-image-picker';
+
+import {data} from './data';
+
+const {width} = Dimensions.get('screen');
+
+const ITEM_WIDTH = width * 0.9;
+const ITEM_HEIGHT = ITEM_WIDTH * 0.9;
+
+const Home = () => {
+  const tabBarheight = useBottomTabBarHeight();
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Home</Text>
+      </View>
+
+      <TouchableOpacity
+        onPress={async () => {
+          const result = await launchImageLibrary({
+            mediaType: 'video',
+          });
+          console.log('result', result)
+          showEditor(result?.assets[0]?.uri , {
+                maxDuration: 20,
+              })
+                .then((res) => console.log(res))
+                .catch((e) => console.log(e));
+
+         
+        }}
+        style={{ padding: 10, backgroundColor: 'red' }}
+      >
+        <Text style={{ color: 'white' }}>Launch Library</Text>
+      </TouchableOpacity>
+
+
+      {/* Scrollable Content */}
+      <View style={styles.scrollContainer}>
+        <ScrollView
+          indicatorStyle="white"
+          contentContainerStyle={[
+            styles.scrollContentContainer,
+            {
+              paddingBottom:
+                Platform.OS === 'ios' ? tabBarheight : tabBarheight - 40,
+            },
+          ]}>
+          {data.map((item) => (
+            <View key={item.id} style={styles.imageContainer}>
+              <Image
+                style={styles.imageCard}
+                source={{uri: item.image_url}}
+                resizeMode="cover"
+              />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f0f0f',
+  },
+  contentContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  scrollContentContainer: {
+    alignItems: 'center',
+  },
+  imageContainer: {
+    marginBottom: 14,
+  },
+  imageCard: {
+    borderRadius: 14,
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+  },
+});
+
+export default Home;
